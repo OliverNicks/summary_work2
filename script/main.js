2d8fa75157e47202587e1f4cfe6bb6758a30616a
Oleg <86343004+OliverNicks@users.noreply.github.com>
2022-11-10 20:13:32 +0200

    FirstComit
    Without node mmodules

---
+++ script/main.js
@@ -0,0 +1,24 @@
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+// slider
